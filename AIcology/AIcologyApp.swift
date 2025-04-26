12674135a95673dd14a99d2cd1745c7c889499c5
alessiorubicini <alessiorubicini05@gmail.com>
2025-04-26 22:16:14 +0200

    Initial Commit

---
+++ AIcology/AIcologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIcologyApp.swift
+//  AIcology
+//
+//  Created by Alessio Rubicini on 26/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AIcologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
